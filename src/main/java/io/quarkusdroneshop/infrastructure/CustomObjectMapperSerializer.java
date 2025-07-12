d7b12ee9ef652ec77a15f6eaa6de91dba5219f0c
Noriaki Mushino <nmushino@redhat.com>
2025-07-12 17:26:01 +0900

    Debug

--- src/main/java/io/quarkusdroneshop/infrastructure/CustomObjectMapperSerializer.java
+++ src/main/java/io/quarkusdroneshop/infrastructure/CustomObjectMapperSerializer.java
@@ -2,17 +2,19 @@ package io.quarkusdroneshop.infrastructure;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.quarkus.kafka.client.serialization.ObjectMapperSerializer;
 
 public class CustomObjectMapperSerializer<T> extends ObjectMapperSerializer<T> {
+
     public CustomObjectMapperSerializer() {
-        super(createCustomObjectMapper());
+        super(createObjectMapper());
     }
 
-    private static ObjectMapper createCustomObjectMapper() {
+    private static ObjectMapper createObjectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.findAndRegisterModules(); // JavaTimeModule を登録
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // ISO-8601 形式にする
+        mapper.registerModule(new JavaTimeModule()); // for java.time.Instant
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // ⬅️ ISO-8601 にする
         return mapper;
     }
 }
